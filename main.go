108c6b286be1ffa04891684c0000a36fdfd6f316
dangwan <wanchundang@gmail.com>
2022-10-20 11:25:56 +0800

    feat: add request forward demo

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"net/url"
+	"strings"
 
+	"github.com/pkg/errors"
+)
+
+func handleClientRequest(conn net.Conn) error {
+	if conn == nil {
+		return errors.Wrap(nil, "Client conn is nil")
+	}
+	defer conn.Close()
+	var b [1024]byte
+	n, err := conn.Read(b[:])
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	var method, host string
+	fmt.Sscanf(string(b[:bytes.IndexByte(b[:], '\n')]), "%s%s", &method, &host)
+	reqUrl, err := url.Parse(host)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	address := reqUrl.Host
+	if reqUrl.Opaque == "443" {
+		address = reqUrl.Scheme + ":443"
+	} else {
+		if !strings.Contains(reqUrl.Host, ":") {
+			address = reqUrl.Host + ":80"
+		}
+	}
+	svr, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	if method == "connect" {
+		fmt.Fprint(conn, "http:/1.1 200 connection established\r\n")
+	} else {
+		svr.Write(b[:n])
+	}
+	go io.Copy(svr, conn)
+	io.Copy(conn, svr)
+	return nil
+}
 func main() {
-	fmt.Println("Start now")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Start listen 8080")
+	for {
+		client, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleClientRequest(client)
+
+	}
 }
